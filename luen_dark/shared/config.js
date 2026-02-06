a6337f76de054365f3f820929831e5d01c2baa2c
luenai <skdksehff444666@gmail.com>
2026-02-06 19:39:17 +0900

    Update config.js

--- luen_dark/shared/config.js
+++ luen_dark/shared/config.js
@@ -1,4 +1,4 @@
 // 1) 아래 두 값만 당신 Supabase 프로젝트 값으로 유지/수정하세요.
 // - Project Settings > API 에서 확인 가능
-window.NEO_SUPABASE_URL = 'https://uvwturgomovjvokojtkp.supabase.co';
-window.NEO_SUPABASE_ANON_KEY = 'sb_publishable_a6k4T2_-ksuHVUvCduUlCQ_DtoR9R4F';
+window.LUEN_SUPABASE_URL = 'https://bbpythvxxjmagpnrxqxx.supabase.co';
+window.LUEN_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJicHl0aHZ4eGptYWdwbnJ4cXh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwOTg2MjUsImV4cCI6MjA4NTY3NDYyNX0.wz8yp16sHIoewwAC8jlOUfEPN5OR-IHamUs03vlgLsk';
